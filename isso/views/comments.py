1d8791daa491470eedb42d56a991bcc34422111c
hscpro <huisc@foxmail.com>
2017-11-12 14:59:45 +0800

    Squashed commit of the following:
    commit bd44d4247c8e0be6b8611431cf521fe88f8545a2
    Author: Tim DuBois <tim@neophilus.net>
    Date:   Fri Apr 21 00:18:10 2017 +0200
    
        Highlighting for author comments, author can be set in isso.conf
    
    # Conflicts:
    #	isso/js/app/text/comment.jade

--- isso/views/comments.py
+++ isso/views/comments.py
@@ -83,6 +83,7 @@ class API(object):
         ('new',     ('POST', '/new')),
         ('count',   ('GET', '/count')),
         ('counts',  ('POST', '/count')),
+        ('author',  ('GET', '/author')),
         ('view',    ('GET', '/id/<int:id>')),
         ('edit',    ('PUT', '/id/<int:id>')),
         ('delete',  ('DELETE', '/id/<int:id>')),
@@ -493,6 +494,15 @@ class API(object):
             raise BadRequest("JSON must be a list of URLs")
 
         return JSON(self.comments.count(*data), 200)
+
+    def author(self, environ, request):
+
+        rv = self.isso.author
+
+        if rv == "":
+            raise NotFound
+
+        return JSON(rv, 200)
 
     def preview(self, environment, request):
         data = request.get_json()
