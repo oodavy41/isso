6ce9c661f6565087c15282676d0c696ac29118db
hscpro <huisc@foxmail.com>
2017-11-13 21:51:44 +0800

    F BUG

--- isso/js/app/isso.js
+++ isso/js/app/isso.js
@@ -72,7 +72,7 @@ define(["app/dom", "app/utils", "app/config", "app/api", "app/jade", "app/i18n",
                 author: author, email: email, website: website,
                 text: utils.text($(".textarea", el).innerHTML),
                 parent: parent || null,
-                notification: $("[name=notification]", el).checked ? 1 : 0,
+                subscribe: $("[name=subscribe]", el).checked ? 1 : 0,
                 title: $("#isso-thread").getAttribute("data-title") || null
             }).then(function(comment) {
                 $(".textarea", el).innerHTML = "";
